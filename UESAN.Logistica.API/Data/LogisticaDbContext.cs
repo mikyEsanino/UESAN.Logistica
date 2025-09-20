333f4040df372118ca6bf86729c2e3d248cd44d9
MiguelAL <23100173@ue.edu.pe>
2025-09-19 20:47:46 -0500

    Agregar archivos de proyecto.

---
+++ UESAN.Logistica.API/Data/LogisticaDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace UESAN.Logistica.API.Data;
+
+public partial class LogisticaDbContext : DbContext
+{
+    public LogisticaDbContext()
+    {
+    }
+
+    public LogisticaDbContext(DbContextOptions<LogisticaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Product { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=archivaldo;Database=logisticaDB;Integrated Security=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasMaxLength(50)
+                .IsFixedLength()
+                .HasColumnName("description");
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ UESAN.Logistica.API/Program.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using UESAN.Logistica.API.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var _config = builder.Configuration;
+var cnx = _config.GetConnectionString("DevConnection");
+
+builder
+    .Services
+    .AddDbContext<LogisticaDbContext>
+    (options => options.UseSqlServer(cnx));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
